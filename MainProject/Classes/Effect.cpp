be60c75dedc16b9aa8fe9e2e95a6582602afed5a
SPINEL\miyak <subadresstmg@gmail.com>
2025-06-04 15:04:12 +0900

    effectの位置を変更

--- MainProject/Classes/Effect.cpp
+++ MainProject/Classes/Effect.cpp
@@ -15,7 +15,7 @@ void Effect::Initialize()
 {
 
     sprite_.params.siz = Math::Vector2(40.0f, 83.0f);
-    sprite_.params.pos = Math::Vector2(-140.0f, 0.0f);
+    sprite_.params.pos = Math::Vector2(710.0f, 437.0f);
     sprite_.params.enableDrawRect(Rectf(
         0, 0, sprite_.params.siz.x, sprite_.params.siz.y
     ));
